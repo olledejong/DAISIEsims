34246d01d80b38d62e4e5ee7862042462799184e
Olle de Jong <olledejong@gmail.com>
2023-09-15 11:40:26 +0200

    Addition of rate tests

--- tests/testthat/test-DAISIE_rates.R
+++ tests/testthat/test-DAISIE_rates.R
@@ -58,13 +58,13 @@ test_that("Test C++ DAISIE_rates; island area (constant area)", {
     )
 
     expect_true(
-        sum(rates$immig_rate, rates$ext_rate,rates$ana_rate, rates$clado_rate) > 0
+        sum(rates$immig_rate, rates$ext_rate, rates$ana_rate, rates$clado_rate) > 0
     )
     expect_false(
-        rates$immig_rate < 0 && rates$ext_rate &&rates$ana_rate && rates$clado_rate
+        rates$immig_rate < 0 && rates$ext_rate && rates$ana_rate && rates$clado_rate
     )
 
-    # TODO Somehow test the correctness of the rates them selfs.
+    # TODO Somehow test the correctness of the rates themselves.
     # NOTE We cannot, however, access the functions that calculate the rates.
     # NOTE We only have the updated rates for a specific time-point.
 })
@@ -99,3 +99,106 @@ test_that("Test C++ DAISIE_rates; island area (max area too large)", {
 })
 
 # NOTE not testing ontogeny & sea_level within island area (not implemented yet)
+
+test_that("Test C++ DAISIE_rates; island area (max area too large)", {
+    compare_rates <- function(gam = 1, laa = 1, lac = 1, mu = 1) {
+        timeval <- 2
+        total_time <- 5
+        mainland_n <- 100
+        set.seed(5)
+
+        area_pars <- create_area_pars(
+            max_area = 1,
+            current_area = 1,
+            proportional_peak_t = 0,
+            total_island_age = 0,
+            sea_level_amplitude = 0,
+            sea_level_frequency = 0,
+            island_gradient_angle = 0
+        )
+
+        nonoceanic_pars <- c(0, 0)
+        hyper_pars <- create_hyper_pars(d = 0, x = 0)
+
+        nonoceanic_sample <- DAISIE_nonoceanic_spec(
+            prob_samp = nonoceanic_pars[1],
+            prob_nonend = nonoceanic_pars[2],
+            mainland_n = mainland_n
+        )
+        maxspecID <- mainland_n
+        island_spec <- c()
+        stt_table <- matrix(ncol = 4)
+        colnames(stt_table) <- c("Time", "nI", "nA", "nC")
+        spec_tables <- DAISIE_spec_tables(
+            stt_table,
+            total_time,
+            timeval,
+            nonoceanic_sample,
+            island_spec,
+            maxspecID
+        )
+
+        island_spec <- spec_tables$island_spec
+        num_spec <- length(island_spec[, 1])
+        num_immigrants <- length(which(island_spec[, 4] == "I"))
+
+        rates_cpp <- update_rates_cpp(
+            timeval, # timeval
+            total_time, # total_time
+            gam, # immig rate
+            laa, # anagenesis rate
+            lac, # caldogenesis rate
+            mu, # extinction rate
+            hyper_pars,
+            area_pars,
+            4, # carrying cap
+            num_spec, # current num of species
+            num_immigrants, # number of non-endemic species
+            1, # number of mainland species
+            0, # peak
+            0, # island ontogeny
+            0 # sea level
+        )
+        rates_r <- update_rates(
+            timeval = timeval,
+            total_time = total_time,
+            gam = gam,
+            laa = laa,
+            lac = lac,
+            mu = mu,
+            hyper_pars = hyper_pars,
+            area_pars = area_pars,
+            K = 4,
+            num_spec = num_spec,
+            num_immigrants = num_immigrants,
+            mainland_n = 1,
+            extcutoff = NULL,
+            island_ontogeny = 0,
+            sea_level = 0
+        )
+
+        testthat::expect_equal(rates_cpp$immig_rate, rates_r$immig_rate)
+        testthat::expect_equal(rates_cpp$ana_rate, rates_r$ana_rate)
+        testthat::expect_equal(rates_cpp$clado_rate, rates_r$clado_rate)
+        testthat::expect_equal(rates_cpp$ext_rate, rates_r$ext_rate)
+    }
+
+
+    for (incr in seq(0, 1, by = 0.1)) {
+        compare_rates(gam = incr)
+        compare_rates(laa = incr)
+        compare_rates(lac = incr)
+        compare_rates(mu = incr)
+    }
+})
+
+# test_that("Microbenchmarking update_rates function", {
+
+#   r_version <- function() {
+
+#   }
+  
+#   cpp_version <- function() {
+      
+#   }
+# })
