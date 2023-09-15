433ec9c90a94219d6da59a175996fce9abb5b050
Olle de Jong <olledejong@gmail.com>
2023-09-15 13:48:28 +0200

    Microbenchmark of update_rates versions

--- tests/testthat/test-DAISIE_rates.R
+++ tests/testthat/test-DAISIE_rates.R
@@ -192,13 +192,89 @@ test_that("Test C++ DAISIE_rates; compare cpp rate outcomes to original rate out
     }
 })
 
-# test_that("Microbenchmarking update_rates function", {
+test_that("Microbenchmarking update_rates function", {
+      timeval <- 2
+        total_time <- 5
+        mainland_n <- 100
+        set.seed(5)
 
-#   r_version <- function() {
+        area_pars <- create_area_pars(
+            max_area = 1,
+            current_area = 1,
+            proportional_peak_t = 0,
+            total_island_age = 0,
+            sea_level_amplitude = 0,
+            sea_level_frequency = 0,
+            island_gradient_angle = 0
+        )
 
-#   }
-  
-#   cpp_version <- function() {
-      
-#   }
-# })
+        nonoceanic_pars <- c(0, 0)
+        hyper_pars <- create_hyper_pars(d = 0, x = 0)
+
+        nonoceanic_sample <- DAISIE_nonoceanic_spec(
+            prob_samp = nonoceanic_pars[1],
+            prob_nonend = nonoceanic_pars[2],
+            mainland_n = mainland_n
+        )
+        maxspecID <- mainland_n
+        island_spec <- c()
+        stt_table <- matrix(ncol = 4)
+        colnames(stt_table) <- c("Time", "nI", "nA", "nC")
+        spec_tables <- DAISIE_spec_tables(
+            stt_table,
+            total_time,
+            timeval,
+            nonoceanic_sample,
+            island_spec,
+            maxspecID
+        )
+
+        island_spec <- spec_tables$island_spec
+        num_spec <- length(island_spec[, 1])
+        num_immigrants <- length(which(island_spec[, 4] == "I"))
+
+    update_rates_cpp_version <- function() {
+        rates_cpp <- update_rates_cpp(
+            timeval, # timeval
+            total_time, # total_time
+            1.0, # immig rate
+            1.0, # anagenesis rate
+            1.0, # caldogenesis rate
+            0.1, # extinction rate
+            hyper_pars,
+            area_pars,
+            4, # carrying cap
+            num_spec, # current num of species
+            num_immigrants, # number of non-endemic species
+            1, # number of mainland species
+            0, # peak
+            0, # island ontogeny
+            0 # sea level
+        )
+    }
+
+    update_rates_r_version <- function() {
+        rates_r <- update_rates(
+            timeval = timeval,
+            total_time = total_time,
+            gam = 1.0, # immig rate
+            laa = 1.0, # anagenesis rate
+            lac = 1.0, # caldogenesis rate
+            mu = 0.1, # extinction rate
+            hyper_pars = hyper_pars,
+            area_pars = area_pars,
+            K = 4,
+            num_spec = num_spec,
+            num_immigrants = num_immigrants,
+            mainland_n = 1,
+            extcutoff = NULL,
+            island_ontogeny = 0,
+            sea_level = 0
+        )
+    }
+
+    res <- microbenchmark::microbenchmark(update_rates_cpp_version(), update_rates_r_version())
+    print(res)
+    
+    expect_true(mean(res$time[res$expr == "update_rates_cpp_version()"]) < mean(res$time[res$expr == "update_rates_r_version()"]))
+})
